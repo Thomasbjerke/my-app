323557ff5a3c9ff81482b477a083de456ed1a712
thombje <thombje@ntnu.no>
2022-01-17 15:57:13 +0100

    initial commit (includes create-react-app and adressaparkenService

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AdressaparkenService from "./services/AdressaparkenService";
 
 function App() {
+  let adressaparkenService = new AdressaparkenService();
+  adressaparkenService.getSensorData().then((res: any) => {
+    console.log(res);
+    console.log(res[0].value);
+  });
+  //console.log("test i app.tsx", data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Test</p>
     </div>
   );
 }
---
+++ src/services/AdressaparkenService.tsx
@@ -0,0 +1,37 @@
+import qs from 'qs';
+
+
+/**
+ * @class FitbitService
+ * FitbitService is a service class created to provide communication with
+ * the Fitbit Web API via both OAuth authentication and REST web endpoints.
+ * Credentials for authentication are to be found in the config.js file.
+ */
+class AdressaparkenService {
+    getSensorData(): any {
+        return new Promise((resolve, reject) => {
+
+            fetch('https://api.adressaparken.no/v1/sensorDataList?sensor=tc&limit=10&offset=0', {
+                method: 'GET',
+                headers: {
+
+                }
+            })
+                .then(response => {
+                    if (!response.ok) {
+                        return reject(response);
+                    }
+                    return response.json();
+                })
+                .then(json => {
+                    return resolve(json);
+                })
+                .catch(error => { return reject('Fetch request failed: ' + error) });
+        });
+    }
+
+
+}
+
+
+export default AdressaparkenService;
